1bb73375326ab8399d1237987c9657ccbebd5fd2
yeliz durmus <durmus.yeliz95@gmail.com>
2020-05-28 17:14:51 +0300

    indexFactory socket connection method

---
+++ public/javascripts/controller/indexController.js
@@ -0,0 +1,12 @@
+app.controller('indexController',['$scope','indexFactory',($scope,indexFactory)=>{
+    const connectionOptions={
+        reconnectionAttempts:3,
+        reconnectionDelay:600
+    };
+    indexFactory.connectSocket('http://localhost:3000',connectionOptions)
+        .then((socket)=>{
+            console.log('bağlantı gerçekleşti',socket);
+        }).catch((err)=>{
+            console.log(err);
+        });
+}]);
